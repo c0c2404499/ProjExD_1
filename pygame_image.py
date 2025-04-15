72ce1058445ab569b08898db93801051b8fcc255
c0c2404499 <c0c2404499@edu.teu.ac.jp>
2025-04-15 17:40:55 +0900

    演習課題1

--- pygame_image.py
+++ pygame_image.py
@@ -13,33 +13,34 @@ def main():
     bg_img2 = pg.transform.flip(bg_img, True, False) #反転
     kokaton_img = pg.image.load("fig/3.png")
     kokaton_img = pg.transform.flip(kokaton_img, True, False)
-    kokaton_rect = kokaton_img.get_rect()
-    kokaton_rect.center = 300, 200
+    kokaton_rect = kokaton_img.get_rect() #rectを取得
+    kokaton_rect.center = 300, 200 #中心座標を設定
     
     
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        key_lst = pg.key.get_pressed()
-        if key_lst[pg.K_UP]:
-            kokaton_rect.move_ip((0, -1))
-        if key_lst[pg.K_DOWN]:
+        key_lst = pg.key.get_pressed() #キーの押下状態のリストを取得
+        if key_lst[pg.K_UP]: #上矢印キーが押されたとき
+            kokaton_rect.move_ip((0, -1)) #横に0、縦に―1移動させる
+        elif key_lst[pg.K_DOWN]:
             kokaton_rect.move_ip(0, +1)
-        if key_lst[pg.K_LEFT]:
-            kokaton_rect.move_ip(-1, 0)
-        if key_lst[pg.K_RIGHT]:
+        elif key_lst[pg.K_RIGHT]:
             kokaton_rect.move_ip(+1, 0)
+        else:
+            kokaton_rect.move_ip(-1, 0) #演習課題1
+        
 
         x = tmr % 3200
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_img2, [-x + 1600, 0])
         screen.blit(bg_img, [-x + 3200, 0])
-        #screen.blit(kokaton_img, [300, 200])
-        screen.blit(kokaton_img, kokaton_rect)
+        #screen.blit(kokaton_img, [300 -x, 200])
+        screen.blit(kokaton_img, kokaton_rect) #画像をrectに従って貼り付け
         pg.display.update()
         tmr += 1
-        clock.tick(400)
+        clock.tick(200)
 
 
 if __name__ == "__main__":
