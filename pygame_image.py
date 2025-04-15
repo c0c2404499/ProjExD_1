64b57d8a8c808a497b389ab4d97943ba60af0817
c0c2404499 <c0c2404499@edu.teu.ac.jp>
2025-04-15 16:02:47 +0900

    ２枚目の背景画像を反転して貼り付け

--- pygame_image.py
+++ pygame_image.py
@@ -9,9 +9,11 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
-    bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img = pg.image.load("fig/pg_bg.jpg") #背景画像を挿入
+    bg_img = pg.transform.flip(bg_img, True, False) #反転
     kokaton_img = pg.image.load("fig/3.png")
     kokaton_img = pg.transform.flip(kokaton_img, True, False)
+    
     tmr = 0
     while True:
         for event in pg.event.get():
@@ -20,7 +22,7 @@ def main():
         x = tmr
         screen.blit(bg_img, [-x, 0])
         screen.blit(kokaton_img, [300, 200])
-        screen.blit(bg_img, [1600, 0])
+        screen.blit(bg_img, [-x + 1600, 0])
         pg.display.update()
         tmr += 1
         clock.tick(400)
