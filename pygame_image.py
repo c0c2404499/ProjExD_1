43e86365ca67797b2c84aca622014a9c9deb5f4b
c0c2404499 <c0c2404499@edu.teu.ac.jp>
2025-04-15 16:45:54 +0900

    こうかとんを動かす

--- pygame_image.py
+++ pygame_image.py
@@ -13,18 +13,30 @@ def main():
     bg_img2 = pg.transform.flip(bg_img, True, False) #反転
     kokaton_img = pg.image.load("fig/3.png")
     kokaton_img = pg.transform.flip(kokaton_img, True, False)
+    kokaton_rect = kokaton_img.get_rect()
+    kokaton_rect.center = 300, 200
+    
     
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kokaton_rect.move_ip((0, -1))
+        if key_lst[pg.K_DOWN]:
+            kokaton_rect.move_ip(0, +1)
+        if key_lst[pg.K_LEFT]:
+            kokaton_rect.move_ip(-1, 0)
+        if key_lst[pg.K_RIGHT]:
+            kokaton_rect.move_ip(+1, 0)
 
-        x = tmr
         x = tmr % 3200
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_img2, [-x + 1600, 0])
         screen.blit(bg_img, [-x + 3200, 0])
-        screen.blit(kokaton_img, [300, 200])
+        #screen.blit(kokaton_img, [300, 200])
+        screen.blit(kokaton_img, kokaton_rect)
         pg.display.update()
         tmr += 1
         clock.tick(400)
