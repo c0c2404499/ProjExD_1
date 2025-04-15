6c18cc6114c67223aed6937c09170b4d62805613
c0c2404499 <c0c2404499@edu.teu.ac.jp>
2025-04-15 15:51:14 +0900

    こうかとん左右反転、FPS200に変更、こうかとん動かす

--- pygame_image.py
+++ pygame_image.py
@@ -10,16 +10,19 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
-    kokaton_image = pg.image.load("fig/3.png")
+    kokaton_img = pg.image.load("fig/3.png")
+    kokaton_img = pg.transform.flip(kokaton_img, True, False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
+        x = tmr
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(kokaton_img, [300, 200])
         pg.display.update()
-        tmr += 1        
-        clock.tick(10)
+        tmr += 1
+        clock.tick(200)
 
 
 if __name__ == "__main__":
